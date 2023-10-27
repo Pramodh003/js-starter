20839d6580710b4a5ee90f440ef1fa675693b3f4
Pramodh BR <72683141+Pramodh003@users.noreply.github.com>
2023-10-27 01:03:51 +0000

    Conversion type

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,33 @@
+// let score = true
+
+// const {score} = req.body
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*
+ "33" => 33
+ "33abc" => NaN
+ true => 1; false = 0
+ 
+*/
+
+// let isLogged = "akash"
+
+// let bolleanIsLoggedIn = Boolean(isLogged)
+// console.log(bolleanIsLoggedIn);
+// console.log(typeof(bolleanIsLoggedIn));
+
+// 1=> true
+// 0 => False
+// "" => False
+// "akash" => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
