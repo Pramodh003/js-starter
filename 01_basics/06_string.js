6566fce1e47cdb84340429b26a370ef6c2998e2b
Pramodh BR <72683141+Pramodh003@users.noreply.github.com>
2023-10-28 14:39:40 +0000

    added string math operations

---
+++ 01_basics/06_string.js
@@ -0,0 +1,33 @@
+const name = "hitesh "
+const repoCOunt = 50
+
+// console.log(name + repoCOunt +" Value");
+
+console.log(`Hi my name is pramodh ${name} and my repo count is ${repoCOunt}`);
+
+const gameName = new String('pramodh-br')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('m'));
+
+// const sliece = gameName.substring(1,4)
+// console.log(sliece)
+// const sliece1 = gameName.slice(-8,4)
+// console.log(sliece1)
+// const newString = "   pramodh   "
+// console.log(newString); 
+// console.log(newString.trim());
+
+// const url = "https://pramodh.com/pramodh%20br"
+
+// console.log(url.replace('%20','-'))
+
+// console.log(url.includes('akah'));
+
+const newstring = new String("pramodh-br-akash");
+console.log(newstring.split("-"));
---
+++ 01_basics/07_num_and_math.js
@@ -0,0 +1,36 @@
+// const score = 400
+
+// const banking = new Number(400)
+// console.log(banking);
+// console.log(typeof banking.toString().length);
+// console.log(banking.toString().length);
+// console.log(banking.toFixed(2));
+
+// const othernumber = 1123.8966
+
+// console.log(othernumber.toPrecision(3));
+
+// const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// **************Maths**********************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.round(4.8));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(3.2));
+// console.log(Math.min(4.3,5,6,7,8));
+// console.log(Math.max(9.3,5,6,7,8));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
