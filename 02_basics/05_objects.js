761b5761541ce78bf07581a2b6680d102fc1e137
Pramodh BR <72683141+Pramodh003@users.noreply.github.com>
2023-11-02 16:55:48 +0000

    added object code basics

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,11 @@
+const course =  {
+    courseName: "Js in english",
+    price: "999",
+    CourseInstructor: "Pramodh"
+}
+
+// course.CourseInstructor
+
+const {CourseInstructor} = course
+
+console.log(CourseInstructor);
