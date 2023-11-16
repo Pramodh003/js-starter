f2286493a8f61d9fcc01d5a0fc3e9150258ab8b5
Pramodh BR <72683141+Pramodh003@users.noreply.github.com>
2023-11-16 17:43:37 +0000

    add code

--- 05_iterations/05_for_each.js
+++ 05_iterations/05_for_each.js
@@ -1,5 +1,42 @@
 const coding = ["js","ruby","java","python","cpp"]
 
-coding.forEach( function (item) {
-    
+// coding.forEach( function (item) {
+//     console.log(item);
+// })
+
+// coding.forEach((val) => {
+//     console.log(val);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach((item,index,arr) => {
+//     console.log(item,index,arr);
+// })
+
+const myCoding = [
+    {
+        languageName: "JavaScript",
+        languageFile: "JS"
+    },
+    {
+        languageName: "Python",
+        languageFile: "PY"
+    },
+    {
+        languageName: "Java",
+        languageFile: "JV"
+    },
+    {
+        languageName: "Golang",
+        languageFile: "Go"
+    },
+]
+
+myCoding.forEach( (key,val) => {
+    console.log(key.languageName);
 })
---
+++ 05_iterations/06_filter_map.js
@@ -0,0 +1,11 @@
+// const coding = ["js","python","java","cpp","ruby"]
+
+// const values = coding.forEach( (item) => {
+//     // console.log(item);
+// })
+
+// console.log(values);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+myNums.filter( (num) => num > 4 )
